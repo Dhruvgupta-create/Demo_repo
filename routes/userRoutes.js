0d5f609e08d0beaddfdcf0e49ac726dd1ca8427b
Dhruv gupta <dgdhruvgupta0408@gmail.com>
2024-07-11 23:11:49 +0530

    authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { updateUser,getAllUser,getSingleUser,deleteUser } from "../controllers/userController.js";
+import { authenticate } from "../auth/verifyToken.js";
 
 const router=express.Router();
-router.put("/updateUser/:id",updateUser)
+router.put("/updateUser/:id",authenticate,updateUser)
 router.get("/getAllUser",getAllUser)
 router.get("/getSingleUser/:id",getSingleUser)
 router.delete("/deleteUser/:id",deleteUser)
