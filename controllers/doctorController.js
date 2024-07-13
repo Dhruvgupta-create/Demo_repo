4b055d522aa55057981a80cb5e455116888f6375
Dhruv gupta <dgdhruvgupta0408@gmail.com>
2024-07-13 20:17:11 +0530

    review handler

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -30,7 +30,7 @@ export const getSingleDoctor = async(req,res)=> {
     const id=req.params.id;
 
     try {         
-         const singleDoctor=await Doctor.findById(id).select("-password")
+         const singleDoctor=await Doctor.findById(id).populate('reviews').select("-password")
 
          res.status(200).json({success:true , message:'Doctor Found',data:singleDoctor})
     } catch (error) {
--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,8 +1,12 @@
 import express from "express"
 import { updateDoctor,deleteDoctor,getAllDoctor,getSingleDoctor } from "../controllers/doctorController.js";
 import { authenticate,restrict } from "../auth/verifyToken.js";
+import reviewRouter from "./reviewRoutes.js";
 
 const router=express.Router();
+
+//nested routes
+router.use("/:doctorId/reviews",reviewRouter);
 router.put("/updateDoctor/:id",authenticate,restrict(['doctor']),updateDoctor)
 router.get("/getAllDoctor",getAllDoctor)
 router.get("/getSingleDoctor/:id",getSingleDoctor)
